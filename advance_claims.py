7a98699f68f47f4a7288fbbf741bcac55a43fe2c
innerstage <mbarbierif@gmail.com>
2020-05-28 10:19:10 -0400

    May 28th latest update

--- advance_claims.py
+++ advance_claims.py
@@ -4,7 +4,7 @@ import us
 import numpy as np
 
 ### CHANGE THE LAST WEEK OF INFORMATION HERE:
-last_week = "2020-05-16"
+last_week = "2020-05-23"
 #############################################
 
 weeks = pd.date_range(end=last_week, periods=2, freq='7D')
